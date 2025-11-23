5af7314435fce8ebaa927547aecc2cba1ce076c0
User <1@chaika.team>
2025-11-24 02:25:06 +0300

    fix(upload): disable SSL verification for S3 client to resolve TLS error

--- app/services/upload_service.py
+++ app/services/upload_service.py
@@ -57,7 +57,8 @@ async def upload_event_poster(
             aws_access_key_id=settings.s3_access_key,
             aws_secret_access_key=settings.s3_secret_key,
             region_name=settings.s3_region_name,
-        )
+            verify=False
+        )   
         
         try:
             # Загружаем файл
